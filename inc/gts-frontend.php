88d6c4c5914e3de55f7a38fbbaa9b45e0c42c8cc
seothemes <seothemeswp@gmail.com>
2017-03-19 20:58:29 +0800

    Add schema microdata and front-end hooks.

--- inc/gts-frontend.php
+++ inc/gts-frontend.php
@@ -18,13 +18,52 @@ $loop = new WP_Query( array(
 
 while ( $loop->have_posts() ) : $loop->the_post();
 
-	echo '<li>';
-	if ( has_post_thumbnail() ) {
-		echo the_post_thumbnail( 'gts-thumbnail' );
+	/**
+	 * Opening Markup.
+	 */
+	function gts_markup_open() {
+		echo '<li itemprop="review" itemscope itemtype="http://schema.org/Review">';
 	}
-	echo '<blockquote>' . get_the_content() . '</blockquote>';
-	echo '<h5>' . get_the_title() . '</h5>';
-	echo '</li>';
+
+	/**
+	 * Testimonial Image.
+	 */
+	function gts_image() {
+		if ( has_post_thumbnail() ) {
+			echo the_post_thumbnail( 'gts-thumbnail' );
+		}
+	}
+
+	/**
+	 * Testimonial Content.
+	 */
+	function gts_content() {
+		echo '<blockquote itemprop="reviewBody">' . get_the_content() . '</blockquote>';
+	}
+
+	/**
+	 * Testimonial Title.
+	 */
+	function gts_title() {
+		echo '<h5 itemprop="name">' . get_the_title() . '</h5>';
+	}
+
+	/**
+	 * Closing Markup.
+	 */
+	function gts_markup_close() {
+		echo '</li>';
+	}
+
+	// Add actions to hook.
+	add_action( 'gts', 'gts_markup_open' );
+	add_action( 'gts', 'gts_image' );
+	add_action( 'gts', 'gts_content' );
+	add_action( 'gts', 'gts_title' );
+	add_action( 'gts', 'gts_markup_close' );
+
+	// Run hook.
+	do_action( 'gts' );
 
 endwhile;
 
